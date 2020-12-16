8fffcae3ceb5a44726830a1d319018d81a2789a6
GradedJestRisk <graded.jest.risk@gmail.com>
2020-12-16 09:54:09 +0100

    [PosgreSQL][Testing][JS RPC] Enforce formatting

--- RDBMS/PostgreSQL/testing/js-rpc/server.js
+++ RDBMS/PostgreSQL/testing/js-rpc/server.js
@@ -4,48 +4,46 @@ const Hapi = require('@hapi/hapi');
 const Joi = require('@hapi/joi');
 
 const init = async () => {
+  const defaultPort = 3000;
+  const port = parseInt(process.env.PORT, 10) || defaultPort;
 
-    const defaultPort = 3000;
-    const port = parseInt(process.env.PORT, 10) || defaultPort;
+  const server = Hapi.server({
+    port,
+  });
 
-    const server = Hapi.server({
-        port
-    });
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+      console.log(request.info.remoteAddress + ': ' + request.method.toUpperCase() + ' ' + request.path);
+      return 'Hello World!';
+    },
+  });
 
-    server.route({
-        method: 'GET',
-        path: '/',
-        handler: (request, h) => {
-            console.log(request.info.remoteAddress + ': ' + request.method.toUpperCase() + ' ' + request.path);
-            return 'Hello World!';
-        }
-    });
+  server.route({
+    method: 'PUT',
+    path: '/hello/{name}',
+    config: {
+      validate: {
+        params: Joi.object({
+          name: Joi.string().required(),
+        }),
+      },
+    },
+    handler: (request, h) => {
+      console.log(request.info.remoteAddress + ': ' + request.method.toUpperCase() + ' ' + request.path);
+      return { hello: request.params.name };
+    },
+  });
 
-    server.route({
-        method: 'PUT',
-        path: '/hello/{name}',
-        config: {
-            validate: {
-                params: Joi.object({
-                    name: Joi.string().required()
-                })
-            }
-        },
-        handler: (request, h) => {
-            console.log(request.info.remoteAddress + ': ' + request.method.toUpperCase() + ' ' + request.path);
-            return { hello : request.params.name };
-        }
-    });
+  await server.start();
 
-    await server.start();
-
-    console.log('Server running on %s', server.info.uri);
+  console.log('Server running on %s', server.info.uri);
 };
 
 process.on('unhandledRejection', (err) => {
-
-    console.log(err);
-    process.exit(1);
+  console.log(err);
+  process.exit(1);
 });
 
 init();
