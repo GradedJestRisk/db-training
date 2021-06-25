5e010449041e8429d89743eb0ba7a02e5ad59c91
GradedJestRisk <graded.jest.risk@gmail.com>
2021-06-25 09:32:12 +0200

    [rdbms][postgresql][performance][index] add deactivate index

---
+++ RDBMS/PostgreSQL/performance/use-index-luke/deactivate-indexes/src/deactivate-indexes.js
@@ -0,0 +1,110 @@
+const { Client } = require('pg');
+const { performance } = require('perf_hooks');
+
+const labels = {
+  revert: 'REVERT',
+  createTableWithoutIndex: 'CREATE_TABLE_WITHOUT_INDEX',
+  createIndex: 'CREATE_INDEX',
+  createTableWithIndex: 'CREATE_TABLE_WITH_INDEX',
+  insertData: 'INSERT_DATA',
+};
+
+const revert = async (client) => {
+  await client.query('DROP TABLE IF EXISTS foo', []);
+};
+
+let insertData = async (client) => {
+  await client.query(
+    'INSERT INTO foo (id) VALUES (generate_series( 1, 10000000))'
+  );
+};
+
+const createTableWithoutIndex = async (client) => {
+  await client.query('CREATE TABLE foo (id INTEGER)');
+};
+
+const createIndex = async (client) => {
+  await client.query('ALTER TABLE foo ADD UNIQUE(id)');
+};
+
+const createTableWithIndex = async (client) => {
+  await client.query('CREATE TABLE foo (id INTEGER UNIQUE)', []);
+};
+
+(async () => {
+  const client = new Client({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'database',
+    port: 5432,
+  });
+
+  client.connect();
+  await revert(client);
+
+  // Make measurement more accurate with async hooks
+  // https://blog.logrocket.com/experimental-node-js-testing-the-new-performance-hooks-31fcdd2a747e/
+
+  const afterwardsStart = performance.now();
+
+  console.log('Creating table without index..');
+  console.time(labels.createTableWithoutIndex);
+  await createTableWithoutIndex(client);
+  console.timeEnd(labels.createTableWithoutIndex);
+
+  console.log('Inserting data..');
+  console.time(labels.insertData);
+  await insertData(client);
+  console.timeEnd(labels.insertData);
+
+  console.log('Creating index..');
+  console.time(labels.createIndex);
+  await createIndex(client);
+  console.timeEnd(labels.createIndex);
+
+  const elapsedTimeAfterwards = performance.now() - afterwardsStart;
+
+  console.log('Reverting..');
+  console.time(labels.revert);
+  await revert(client);
+  console.timeEnd(labels.revert);
+
+  const simultaneouslyStart = performance.now();
+
+  console.log('Creating table with index..');
+  console.time(labels.createTableWithIndex);
+  await createTableWithIndex(client);
+  console.timeEnd(labels.createTableWithIndex);
+
+  console.log('Inserting data..');
+  console.time(labels.insertData);
+  await insertData(client);
+  console.timeEnd(labels.insertData);
+
+  const elapsedTimeSimultaneously = performance.now() - simultaneouslyStart;
+
+  console.log('Reverting..');
+  console.time(labels.revert);
+  await revert(client);
+  console.timeEnd(labels.revert);
+
+  console.log(
+    `Total time when index is created after inserting data is ${Math.trunc(
+      elapsedTimeAfterwards
+    )} milliseconds`
+  );
+  console.log(
+    `Total time when index is created before inserting data is ${Math.trunc(
+      elapsedTimeSimultaneously
+    )} milliseconds`
+  );
+
+  const gain =
+    ((elapsedTimeSimultaneously - elapsedTimeAfterwards) /
+      elapsedTimeAfterwards) *
+    100;
+
+  console.log(`Gain to create index afterwards is ${Math.trunc(gain)} %`);
+
+  client.end();
+})();
